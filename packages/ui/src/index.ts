e68a34fb723b4ee0f0bbc4a9402fc66f43053e34
Zander Wilken <wilkenzander007@gmail.com>
2026-02-12 22:31:39 +0200

    feat: build @aic/ui foundation with institutional components and Shadcn-inspired base

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,3 +3,6 @@ export * from './TrustBadge';
 export * from './InstitutionalFooter';
 export * from './Theme';
 export * from './LeadAuditorCertificate';
+export * from './InstitutionalButton';
+export * from './InstitutionalCard';
+export * from './utils';
